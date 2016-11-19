617f4c46491263780d2290ca0eeed91f7a2ace39
Konrad Eisele <eiselekd@gmail.com>
2016-11-19 19:49:31 +0100

    all

--- generate.sh
+++ generate.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -x 
+set -x
 which antlr4
-# 
-antlr4.4.6 -Dlanguage=JavaScript vhdl.g4 -visitor -o lib     -package vhdl
-antlr4.4.6 -Dlanguage=Cpp        vhdl.g4 -visitor -o lib_cpp -package vhdl
-
+#
+antlr4.6 -Dlanguage=JavaScript vhdl.g4 -visitor -o lib     -package vhdl
+antlr4.6 -Dlanguage=Cpp        vhdl.g4 -visitor -o lib_cpp -package vhdl
